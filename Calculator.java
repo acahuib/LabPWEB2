f0635b48e02b6e70f6e33923b1a366a29fb41890
SteveArms <acunoc@unsa.edu.pe>
2024-05-07 17:45:32 -0500

    Creando la clase Calculator

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+//Procedemos a crear la clase Calculator 
+
+public class Calculator {
+    int add(int a, int b){
+        return 0;
+    }
+    int sub(int a, int b){
+        return 0;
+    }
+    int mul(int a, int b){
+        return 0;
+    }
+    int div(int a, int b){
+        return 0;
+    }
+    int mod(int a, int b){
+        return 0;
+    }
+}

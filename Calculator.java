42559ee8c657ccd2a849806df80dac8329dab8be
SteveArms <acunoc@unsa.edu.pe>
2024-05-07 23:22:47 -0500

    Completando el metodo div

--- Calculator.java
+++ Calculator.java
@@ -13,7 +13,12 @@ public class Calculator {
         return r;
     }
     int div(int a, int b){
-        return 0;
+        if(b == 0){
+            // Si en caso cumple la condicion pues el valor que retornara el metodo sera de 0
+            System.out.println("No es posible esta division");
+            a = 0;
+        } 
+        return a / b;
     }
     int mod(int a, int b) {
         if (b == 0) {
